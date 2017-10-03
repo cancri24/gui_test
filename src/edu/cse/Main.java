f467d9adf273a6c757d225e9bdd56b37f3238310
cancri24 <cirwindiehl@gmail.com>
2017-10-03 10:18:11 -0400

    Made the DANGER and DEATH ZONEs

--- src/edu/cse/Main.java
+++ src/edu/cse/Main.java
@@ -1,8 +1,38 @@
 package edu.cse;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage theStage) throws Exception {
+        Label warning = new Label("Please don't click this button");
+        Button dangerous = new Button("ENTER DEATH ZONE");
+        Label reprimand = new Label("HOW COULD YOU");
+
+        VBox pleaseNo = new VBox(10, warning, dangerous);
+
+        Scene firstScene = new Scene(pleaseNo, 200, 75);
+        Scene wrongMove = new Scene(reprimand, 200, 55);
+
+        dangerous.setOnAction(e-> {
+            theStage.setScene(wrongMove);
+            theStage.setTitle("DEATH ZONE");
+        });
+
+        theStage.setScene(firstScene);
+        theStage.setTitle("DANGER ZONE");
+        theStage.show();
+
+    }
+
 
     public static void main(String[] args) {
-	// write your code here
+        Application.launch(args);
     }
 }
